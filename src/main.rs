08c74160b53f3136b7fde24fcab1ed5b138ea369
afriron <afriron@gmail.com>
2021-02-13 21:57:47 +0300

    Init

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Request, Response, Server};
+use std::convert::Infallible;
+use std::net::SocketAddr;
+
+async fn hello_world(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    Ok(Response::new("Hello, World".into()))
+}
+
+#[tokio::main]
+async fn main() {
+    // We'll bind to 127.0.0.1:3000
+    let addr: SocketAddr = SocketAddr::from(([0, 0, 0, 0], 80));
+
+    let make_svc = make_service_fn(|_conn| async {
+        // service_fn converts our function into a `Service`
+        Ok::<_, Infallible>(service_fn(hello_world))
+    });
+
+    // Run this server for... forever!
+    if let Err(e) = Server::bind(&addr).serve(make_svc).await {
+        eprintln!("server error: {}", e);
+    }
+}
